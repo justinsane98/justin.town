d26ab5bcb41ad29ab276caec94ce93a9b5cbd8fb
Justin Shearer <justin.shearer@gmail.com>
2016-01-04 20:36:54 -0700

    added event tracking and changed month

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 $(function() {
+
   if(window.location.hash == '#thanks'){
     $('body').addClass('thanks-modal-active');
     var t;
@@ -6,19 +7,32 @@ $(function() {
       window.history.pushState('', document.title, window.location.pathname);
       $('body').removeClass('thanks-modal-active');
     }, 3000)
+    ga('send', 'event', 'Contact', 'Thanks!');
   }
+
   if(window.location.hash == '#contact'){
       $('body').addClass('contact-modal-active');
+      ga('send', 'event', 'Contact', 'Open Contact Form');
   }
+
   $('.open-contact-form').click(function(e){
     e.preventDefault();
     window.history.pushState('#contact', document.title, window.location.pathname + '#contact');
     $('body').addClass('contact-modal-active');
+    ga('send', 'event', 'Contact', 'Open Contact Form');
   });
 
   $('.close-modal').click(function(e){
     e.preventDefault();
     window.history.back();
     $('body').removeClass('contact-modal-active');
+    ga('send', 'event', 'Contact', 'Close Contact Form');
   });
+
+  $('.contact-form-button').click(function(e){
+    e.preventDefault();
+    $('.contact-form').submit();
+    ga('send', 'event', 'Contact', 'Submit Contact Form');
+  });
+
 });
